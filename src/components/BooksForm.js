44d9d21bfdd4f31e637f7e62e0059cc8862364eb
Azdren <azdreny@gmail.com>
2019-11-07 16:47:53 +0100

    stickler fix

--- src/components/BooksForm.js
+++ src/components/BooksForm.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export function BooksForm(props) {
+export function BooksForm() {
     const categories = ['Action','Biography','History','Horror','Kids','Learning','Sci-Fi']
     return (
         <div>
             <form>
                 <p>
                     <label>Book Title:&nbsp;&nbsp;</label>
-                    <input type="text" onChange={() => console.log('changed')} placeholder="Book title" />
+                    <input type="text" placeholder="Book title" />
                 </p>
                 <p>
                     <label> Pick a category:&nbsp;&nbsp;
-                    <select value="category" onChange={() => console.log('changed') } >
+                    <select value="category">
                     {categories.map(category => {
                      return   <option key={category} value={category}> {category} </option>
                     })}
