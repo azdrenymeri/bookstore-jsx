c7c03af5d6673e963e213c69eb1c683c6abd7684
Azdren <azdreny@gmail.com>
2019-11-07 16:44:31 +0100

    prop validation added for Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {PropTypes} from 'prop-types';
 
 export default function Book(props) {
     return (
@@ -9,3 +9,6 @@ export default function Book(props) {
     );
 }
 
+Book.propTypes = {
+    book: PropTypes.object.isRequired
+}
